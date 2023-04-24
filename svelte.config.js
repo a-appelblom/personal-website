9d77959b537c1ffa8a7dc3015a85a6460f7ae877
a-appelblom <a.appelblom@gmail.com>
2023-04-24 13:00:59 +0200

    4th attempt

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({ postcss: true })],
+	preprocess: [preprocess({ postcss: true }), preprocess({ preserve: ['ld+json'] })],
 
 	kit: {
 		adapter: adapter(),
 		alias: {
 			components: 'src/components/',
-			data: 'src/data/'
-		}
-	}
+			data: 'src/data/',
+		},
+	},
 };
 
 export default config;
