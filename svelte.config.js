9b00ca4336ec6ee2c74ff6e15e71c8d0133efb60
a-appelblom <a.appelblom@gmail.com>
2023-04-24 13:10:29 +0200

    fith attempt

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({ postcss: true }), preprocess({ preserve: ['ld+json'] })],
+	preprocess: [preprocess({ postcss: true, preserve: ['ld+json'] })],
 
 	kit: {
 		adapter: adapter(),
